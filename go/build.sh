a8a88f8278402a3c6b93baf42945baecd9f5e711
Daniel S <daniel.s@foo.bar>
2020-04-19 19:15:24 +0200

    push image to correct repo

--- go/build.sh
+++ go/build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 set -e
 
-APP="golang"
+ORG="acend"
+APP="example-web-go"
 
 # start
-docker build -t $APP .
-bash -c "docker stop $APP; exit 0"
-docker run -d --rm -p 5000:5000 --name $APP $APP
+docker build -t $ORG/$APP .
+bash -c "docker stop $ORG/$APP; exit 0"
+docker run -d --rm -p 5000:5000 --name $APP $ORG/$APP
 sleep 2
 
 # test
 curl -s localhost:5000/
 
 # stop
-#docker logs $APP
-#docker stop $APP
+docker logs $APP
+docker stop $APP
+
+# publish
+docker push $ORG/$APP
 
 # cleanup
 docker image prune --force
