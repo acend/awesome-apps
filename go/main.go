f6b3d16fdd4d7a187b6d6e49bb51a2a0f8c1c8a4
Thomas Philipona <philipona@puzzle.ch>
2020-05-18 20:42:32 +0200

    Add logging and further endpoints

--- go/main.go
+++ go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "os"
     "crypto/rand"
     "encoding/hex"
     "net/http"
@@ -11,11 +12,31 @@ var token,_ = randToken(3)
 
 func main() {
     http.HandleFunc("/", HelloServer)
+    http.HandleFunc("/pod/", PodServer)
+    http.HandleFunc("/health", HealthServer)
+    fmt.Println("acend go app is up and running on host: ", getEnv("HOSTNAME", "not-set"))
     http.ListenAndServe(":5000", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("handling main request with color: ", token)
 	fmt.Fprintf(w, "<h1 style=color:#%s>Hello golang</h1><h2>ID: %s</h2>", token, token)
+}
+
+func PodServer(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("handling pod request")
+	fmt.Fprintf(w, "%s", getEnv("HOSTNAME", "not-set"))
+}
+
+func HealthServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
+
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
 }
 
 func randToken(n int) (string, error) {
